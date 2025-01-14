69aacec7db5ed21a425f8c6f08ed31963d312b4b
angelasandom <angelasandom@gmail.com>
2025-01-14 16:52:18 +0000

    style quiz screen

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { Game } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
+import { Quiz } from './scenes/Quiz';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -24,7 +25,7 @@ const config = {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-  scene: [Boot, Preloader, MainMenu, Game, GameOver],
+  scene: [Boot, Preloader, MainMenu, Game, GameOver, Quiz],
 };
 
 export default new Phaser.Game(config);
