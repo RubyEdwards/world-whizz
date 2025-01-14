a78b0d4de47fa6bdae46a3380d05f531ad7a27d6
charliewlkan <charliewlkan@gmail>
2025-01-14 12:34:37 +0000

    testy-mctest

--- backend/server/controllers/app.controllers.js
+++
@@ -1,3 +0,0 @@
-exports.healthCheck = (req, res) => {
-  res.status(200).send({ msg: 'Health Check: Good' });
-};
--- backend/server/routes/api-router.js
+++
@@ -1,6 +0,0 @@
-import { healthCheck } from '../controllers/app.controllers.js';
-const apiRouter = require('express').Router();
-
-apiRouter.use('/', healthCheck);
-
-export default apiRouter;
