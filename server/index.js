4863adc49923dd8eb4ca3b6ab916605cfc090e04
Toni <dayhlprod@gmail.com>
2025-01-16 15:18:12 +0000

    added travel journal

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-import express from "express";
-import "./loadEnvironment.js";
-import dotenv from "dotenv";
-import connectDB from "../server/db/connection.js";
-import cors from "cors";
-import authRouter from "../server/routes/countries.js";
+import express from 'express';
+import './loadEnvironment.js';
+import dotenv from 'dotenv';
+import connectDB from '../server/db/connection.js';
+import cors from 'cors';
+import authRouter from '../server/routes/countries.js';
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/world-whizz", authRouter);
+app.use('/world-whizz', authRouter);
 
 const port = process.env.PORT || 5000;
 
@@ -21,7 +21,7 @@ const start = async () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
-    console.log("error =>", error);
+    console.log('error =>', error);
   }
 };
 
--- server/validator/user-schema.js
+++ server/validator/user-schema.js
@@ -1,7 +1,7 @@
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const userSchema = new mongoose.Schema(
+const userSchema = new Schema(
   {
     username: {
       type: String,
@@ -18,6 +18,251 @@ const userSchema = new mongoose.Schema(
     profilePicture: {
       type: String,
     },
+    travelJournal: {
+      type: Array,
+      default: [
+        {
+          andorra: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          austria: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          belgium: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          denmark: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          finland: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          france: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          germany: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          greece: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          iceland: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          ireland: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          italy: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          liechtenstein: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          luxembourg: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          malta: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          monaco: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          netherlands: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          norway: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          portugal: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          sanMarino: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          spain: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          sweden: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          switzerland: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          turkey: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+        {
+          unitedKingdom: {
+            isComplete: false,
+            isQ1Correct: false,
+            isQ2Correct: false,
+            isQ3Correct: false,
+            isQ4Correct: false,
+            isQ5Correct: false,
+          },
+        },
+      ],
+    },
   },
   { timestamps: true }
 );
@@ -28,4 +273,4 @@ userSchema.method({
   },
 });
 
-export default mongoose.model("User", userSchema, "users");
+export default mongoose.model('User', userSchema, 'users');
