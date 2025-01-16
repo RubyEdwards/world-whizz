1021036a653b79135c02fcfa72c11c8a995067dc
fdesai60 <fdesai60@gmail.com>
2025-01-16 15:04:35 +0000

    event changed to submit in login

--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -6,19 +6,7 @@ export class Login extends Scene {
   }
 
   create() {
-  
     this.add.image(0, 0, "worldmapbkg").setOrigin(0);
-
-    this.add
-      .text(180, 40, "Login", {
-        fontFamily: "Arial Black",
-        fontSize: 30,
-        color: "#ffffff",
-        stroke: "#127475",
-        strokeThickness: 10,
-      })
-      .setDepth(100)
-      .setOrigin(0.5);
 
     const graphics = this.add.graphics();
     graphics.fillStyle(0xCFE795, 1);
@@ -26,66 +14,52 @@ export class Login extends Scene {
 
     const border = this.add.graphics();
     border.lineStyle(2, 0xA57261, 1);
-    border.strokeRoundedRect(-135, -175, 270, 350); 
+    border.strokeRoundedRect(-135, -175, 270, 350);
 
     const formHtml = this.add.dom(0, 0).createFromCache("loginform");
-   formHtml.setPerspective(800);
+    formHtml.setPerspective(800);
 
-   
     const container = this.add.container(180, 200, [graphics, border, formHtml]);
- 
-   formHtml.addListener("click");
-   formHtml.on("click",  (e)=> {
-      e.preventDefault()
-      if (e.target.id === "loginButton") {
-        const inputUsername = formHtml.getChildByName("username");
-        const inputPassword = formHtml.getChildByName("password");
 
-        if (inputUsername.value !== "" && inputPassword.value !== "") {
-          formHtml.removeListener("click");
+    formHtml.addListener("submit");
+    formHtml.on("submit", (e) => {
+      e.preventDefault();
 
-          const userData = this.registry.get("currUserData")
-          userData.username = inputUsername.value;
-          userData.password = inputPassword.value;
+      const inputUsername = formHtml.getChildByName("username");
+      const inputPassword = formHtml.getChildByName("password");
 
-          this.registry.set("currUserData", userData);
-          
-          // const username = userData.username
-          // const password = userData.password
-          // console.log(username)   
-          // console.log(password)
-        
-          this.scene.start("Game");
+
+      const userData = this.registry.get("currUserData");
+      userData.username = inputUsername.value;
+      userData.password = inputPassword.value;
+
+      this.registry.set("currUserData", userData);
+        // const username = userData.username
+        // const password = userData.password
+        // console.log(username)   
+        // console.log(password)
+      this.scene.start("Game");
       
-        } 
-        else{
-          // handle all info not provided
-        }
-      }
-      else if(e.target.id === "signUpButton"){
-        this.scene.start("SignUp");
-      }
     });
 
+    const signUpButton = formHtml.getChildByName("signUpButton");
+    signUpButton.addEventListener("click", () => {
+      this.scene.start("SignUp");
+    });
+
+  
 
     this.tweens.add({
       targets: container,
-      y: 255, 
+      y: 255,
       duration: 1000,
       ease: "Power3",
     });
 
-  
-    this.mascot = this.add
-      .sprite(0, 500, "mascot1")
+    this.mascot = this.add.sprite(0, 500, "mascot1")
       .setDisplayOrigin(0)
       .setDepth(1)
       .playAfterDelay("blink", Math.random() * 3000);
-
-    this.input.once("pointerdown", () => {
-      this.scene.start("Game");
-    });
-
   }
 
   update() {
