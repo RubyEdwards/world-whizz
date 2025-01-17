bf750787f888278403ddb157d53a74a182ed24be
Toni <dayhlprod@gmail.com>
2025-01-17 11:14:56 +0000

    labelled all endpoints

--- server/routes/countries.js
+++ server/routes/countries.js
@@ -16,13 +16,18 @@ import { signIn, signUp } from '../routes/controllers/auth.controller.js';
 const router = express.Router();
 
 router.get('/countries', getCountries);
+// world-whizz/countries - GETs all countries.
 router.get('/countries/:countrycode', getCountry);
+// world-whizz/countries/IC - GETs country at specified code (Iceland)
 router.get('/countries/:countrycode/quiz', getQuiz);
-
+// world-whizz/countries/IC/quiz - GETs the quiz for the country at specified code (Iceland)
 router.get('/journal', getJournal);
+// world-whizz/journal - currently GETs all country names.
 router.get('/journal/:countryname', getCountryQuizFacts);
-
+// world-whizz/journey/Iceland - GETs all quiz facts for specified country (Iceland)
 router.post('/signup', validateSignUpRequest, isRequestValidated, signUp);
+// world-whizz/signup - POSTs new user to the Users database after being given a body ("usename": "RubyRubyRubyRuby", "password": AaAaAaAa@!1)
 router.post('/signin', validateSignInRequest, isRequestValidated, signIn);
+// world-whizz/signin - POSTs a request to sign into a user if their info is in the database already.
 
 export default router;
